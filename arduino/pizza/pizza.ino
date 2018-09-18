d190444515fdc9d17a7f27ce7a2b56529866a51b
minji7257 <kimmj.95@gmail.com>
2018-09-18 18:08:30 +0900

    code for gripper + refactoring

--- arduino/pizza/pizza.ino
+++ arduino/pizza/pizza.ino
@@ -8,10 +8,26 @@
 MeEncoderOnBoard Encoder_1(SLOT1);
 MeEncoderOnBoard Encoder_2(SLOT2);
 MeEncoderOnBoard Encoder_3(SLOT3);
-MeEncoderOnBoard Encoder_4(SLOT4);
+//MeEncoderOnBoard Encoder_4(SLOT4);
 
-MeMegaPiDCMotor gripper(12);
+//MeMegaPiDCMotor gripper(PORT4B);
+MeDCMotor gripper(12);
 int grip_duration = 100;
+int target_time = millis();
+bool pending_time = false;
+
+bool is_time_out() {
+  if (pending_time && millis() > target_time) {
+    pending_time = false;
+    return true;
+  }
+  return false;
+}
+
+bool set_time_out(float seconds) {
+  pending_time = true;
+  target_time = millis() + seconds * 1000;
+}
 
 void isr_process_encoder1(void)
 {
@@ -39,7 +55,7 @@ void isr_process_encoder3(void)
             Encoder_3.pulsePosPlus();
       }
 }
-
+/*
 void isr_process_encoder4(void)
 {
       if(digitalRead(Encoder_4.getPortB()) == 0){
@@ -48,7 +64,7 @@ void isr_process_encoder4(void)
             Encoder_4.pulsePosPlus();
       }
 }
-
+*/
 void move(int direction, int speed)
 {
       int leftSpeed = 0;
@@ -110,51 +126,57 @@ void setup(){
     TCCR2B = _BV(CS21);
     
     Serial.begin(115200);
-    Serial2.begin(9600);
-    attachInterrupt(Encoder_2.getIntNum(), isr_process_encoder2, RISING);
-    Encoder_2.setPulse(8);
-    Encoder_2.setRatio(46.67);
-    Encoder_2.setPosPid(1.8,0,1.2);
-    Encoder_2.setSpeedPid(0.18,0,0);
-    attachInterrupt(Encoder_3.getIntNum(), isr_process_encoder3, RISING);
-    Encoder_3.setPulse(8);
-    Encoder_3.setRatio(46.67);
-    Encoder_3.setPosPid(1.8,0,1.2);
-    Encoder_3.setSpeedPid(0.18,0,0);
     attachInterrupt(Encoder_1.getIntNum(), isr_process_encoder1, RISING);
     Encoder_1.setPulse(8);
     Encoder_1.setRatio(46.67);
     Encoder_1.setPosPid(1.8,0,1.2);
-    Encoder_1.setSpeedPid(0.18,0,0);    
+    Encoder_1.setSpeedPid(0.18,0,0);  
+    attachInterrupt(Encoder_2.getIntNum(), isr_process_encoder2, RISING);
+    Encoder_2.setPulse(8);
+    Encoder_2.setRatio(46.67);
+    Encoder_2.setPosPid(1.8,0,1.2);
+    Encoder_2.setSpeedPid(0.18,0,0);  
 }
 
 int encoder_1_angle = 0;
 int encoder_2_angle = 0;
-int encoder_3_angle = 0;
 int encoder_1_speed = 8;
 int encoder_2_speed = 0;
-int encoder_3_speed = 30;
+int state = 0;
 
-void set_state_1() {
-  encoder_1_angle = 60;
-  encoder_1_speed = 8;
-  encoder_3_speed = 30;
-  encoder_2_speed = 0;
-  encoder_3_angle = 0;
-}
-void set_state_2() {
-  encoder_1_angle = 0;
-  encoder_1_speed = 8;
-  encoder_3_speed = 30;
-  encoder_2_speed = 150;
-  encoder_3_angle = 200;
-}
-void set_state_0() {
-  encoder_1_angle = 0;
-  encoder_1_speed = 8;
-  encoder_3_speed = 30;
-  encoder_2_speed = 0;
-  encoder_3_angle = 0;
+void set_state(int next_state) {
+  if (state == next_state) {
+    return;
+  }
+
+  switch (next_state) {
+    case 1:
+      encoder_1_angle = 60;
+      encoder_1_speed = 8;
+      encoder_2_speed = 0;
+      Serial.println("state 1");
+      gripper.run(100);
+      break;
+    case 2:
+      encoder_1_angle = 0;
+      encoder_1_speed = 8;
+      encoder_2_speed = 60;
+      if (state == 1) {
+        Serial.println("state 2, gripper run");
+        gripper.run(-100);
+      }
+      break;
+    default:
+      encoder_1_angle = 0;
+      encoder_1_speed = 8;
+      encoder_2_speed = 0;
+      if (state == 1) {
+        Serial.println("state 0, gripper run");
+        gripper.run(-100); 
+      }
+      break;
+  }
+  state = next_state;
 }
 
 void move_to(MeEncoderOnBoard* encoder, int angle, int move_speed) {
@@ -169,69 +191,58 @@ void move_to(MeEncoderOnBoard* encoder, int angle, int move_speed) {
   }
 }
 
+int get_state(int tilt_angle) {
+  if (tilt_angle > 25) {
+    return 1;
+  } else if (tilt_angle < -25) {
+    return 2;
+  }
+  return 0;
+}
+
 void loop(){
   /*
    * Encoder_1: Chili Sauce Motor
    * Encoder_2: Cheese banging Motor
-   * Encoder_3: Cheese lid opening Motor
    */
-   
-  if ((ultrasonic_8.distanceCm()) < 10) {
-    if(Serial.available()){
-        String temp = Serial.readStringUntil('\n');
-        angle = temp.toInt();
-        if(angle > 25) {
-          set_state_1();
-        }
-        else if (angle < -25) {
-          set_state_2();
-        }
-        else {
-          Serial.write("init");
-          set_state_0();
-        }
-    }
-  }
-  else {
-    if(Serial.available()){
-        String temp = Serial.readStringUntil('\n');
-        if(temp != NULL) {
-          angle = temp.substring(1).toInt();
-          if(temp[0] == 'a') {
-            encoder_1_angle += angle;
-            encoder_1_speed = 8;
-          }
-          else if(temp[0] == 'b') {
-            encoder_3_angle += angle;
-            encoder_3_speed = 30;
-            Serial.println(encoder_3_angle);
-          } 
-          else if (temp[0] == 'c') {
-            set_state_0();
-          }
-        }
-    }
-  }
 
+  //gripper.run(250);
+  if (Serial.available()) {
+    String input = Serial.readStringUntil('\n');
+    if (input[0] == 'a' || input[0] == 'b' || input[0] == 'c') {
+      // DEBUG
+      angle = input.substring(1).toInt();
+      switch(input[0]) {
+        case 'a':
+          encoder_1_angle += angle;
+          encoder_1_speed = 8;
+          break;
+        case 'b':
+          set_time_out(0.5);
+          gripper.run(angle);
+          break;
+        case 'c':
+          set_state(0);
+          break;
+      }
+    } else {
+      // RELEASE
+      if (ultrasonic_8.distanceCm() < 10) {
+        angle = input.toInt();
+        int next_state = get_state(angle);
+        set_state(next_state);
+      } else {
+        set_state(0);
+      }
+    }
+  }
+  if (is_time_out()) {
+    Serial.println("time out called");
+    //gripper.stop();
+  }
   move_to(&Encoder_1, encoder_1_angle, encoder_1_speed);
   Encoder_2.runSpeed(encoder_2_speed);
-  move_to(&Encoder_3, encoder_3_angle, encoder_3_speed);
-  move_gripper();
   _loop();
-}
-
-void move_gripper() {
-  
-  if (encoder_1_speed == 0) {
-    grip_duration = 100;
-    return;
-  }
-  
-  _delay(1);
-  int dir = encoder_1_speed > 0 ? 1 : -1;
-  while (grip_duration-- > 0) {
-    gripper.run(100 * dir);
-  }
 }
 
 void _delay(float seconds){
@@ -241,9 +252,6 @@ void _delay(float seconds){
 }
 
 void _loop(){
-    Encoder_2.loop();
-    
-    Encoder_3.loop();
-    
     Encoder_1.loop();
+    Encoder_2.loop();
 }
